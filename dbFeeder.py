99fd24656cf532d16ea3764bd75bd72be53551e3
James Schlitt <jslongboards@gmail.com>
2014-10-02 16:58:50 -0400

    Update dbFeeder.py

--- dbFeeder.py
+++ dbFeeder.py
@@ -1,26 +1,42 @@
-import pymongo as pm
+
 import cPickle
 import sys
-nodes = int(sys.argv[2])
-dbName = sys.argv[1]
+dbType = sys.argv[1]
+nodes = int(sys.argv[3])
+dbName = sys.argv[2]
 files = ['dbFeed/DistDict%s.pickle' % i for i in range(nodes)]
 
-print "Connecting to database:", sys.argv[1]
+print "Connecting to database:", sys.argv[2]
 
-connection = pm.Connection()
-db = connection[dbName]
-posts = db.posts
-
-for dictFile in files:
-    inFile = open(dictFile,'rb')
-    print "Preparing to load file", dictFile, "from pickle"
-    loaded = cPickle.load(inFile); inFile.close()
-    print "Preparing to reindex file",dictFile, "for db insertion"
-    reindexed = [{'key':i[0],'length':i[1]} for i in loaded.items()]
-    del loaded
-    print "Inserting reindexed file", dictFile, "to DB"
-    posts.insert(reindexed)
-    del reindexed
-    print "Operation complete....\n"
+if dbType == 'mongo':
+    import pymongo as pm
+    connection = pm.Connection()
+    db = connection[dbName]
+    posts = db.posts
+    
+    for dictFile in files:
+        inFile = open(dictFile,'rb')
+        print "Preparing to load file", dictFile, "from pickle"
+        loaded = cPickle.load(inFile); inFile.close()
+        print "Preparing to reindex file",dictFile, "for db insertion"
+        reindexed = [{'key':i[0],'length':i[1]} for i in loaded.items()]
+        del loaded
+        print "Inserting reindexed file", dictFile, "to DB"
+        posts.insert(reindexed)
+        del reindexed
+        print "Operation complete....\n"
+        
+elif dbType == 'dbm'
+    import dbm
+    db = dbm.open(dbName, 'c')
+    
+    for dictFile in files:
+        inFile = open(dictFile,'rb')
+        print "Preparing to load file", dictFile, "from pickle"
+        loaded = cPickle.load(inFile); inFile.close()
+        print "Inserting iteratively from file", dictFile, "to DB"
+        for key in loaded.keys()
+            db[key] = loaded[key]
+        del loaded
 
 print "All operations complete"
