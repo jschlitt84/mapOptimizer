71221284082c3e6acadd598e8662e986ebb9a42d
James Schlitt <jslongboards@gmail.com>
2014-10-04 02:01:06 -0400

    Working!

--- dbFeeder.py
+++ dbFeeder.py
@@ -4,7 +4,7 @@ import sys
 dbType = sys.argv[1]
 nodes = int(sys.argv[3])
 dbName = sys.argv[2]
-files = ['%s/DistDict%s.pickle' % (dbName,i) for i in range(nodes)]
+files = ['dbFeed/DistDict%s.pickle' % (i) for i in range(nodes)]
 
 print "Connecting to database:", sys.argv[2]
 
@@ -13,13 +13,14 @@ if dbType == 'mongo':
     connection = pm.Connection()
     db = connection[dbName]
     posts = db.posts
-    
+    found = set()
     for dictFile in files:
         inFile = open(dictFile,'rb')
         print "Preparing to load file", dictFile, "from pickle"
         loaded = cPickle.load(inFile); inFile.close()
+        print "Filtering pre-existing"
         print "Preparing to reindex file",dictFile, "for db insertion"
-        reindexed = [{'key':i[0],'length':i[1]} for i in loaded.items()]
+        reindexed = [{'key':i[0],'length':i[1]} for i in loaded.items() if i[0] not in found]
         del loaded
         print "Inserting reindexed file", dictFile, "to DB"
         posts.insert(reindexed)
@@ -45,16 +46,35 @@ elif dbType == 'mmap':
     def writeIt(key,item):
         return '%s#%s#' % (key,item)
         
+    def insertIntoMmap(offset,data,mapped):
+        global VDATA
+        length = len(data)
+        size = len(mapped)
+        newsize = size + length
+    
+        mapped.flush()
+        mapped.close()
+        f.seek(size)
+        f.write("A"*length)
+        f.flush()
+        VDATA = mmap.mmap(f.fileno(),0)
+    
+        VDATA.move(offset+length,offset,size-offset)
+        VDATA.seek(offset)
+        VDATA.write(data)
+        VDATA.flush()
+        
     import mmap
     found = set()
     print "Opened mmap"
     temp = open(dbName+'.mmap','w')
     temp.write("deboo2014")
     temp.close()
-    with open(dbName+'.mmap', "r+b") as f:
+    with open(dbName+'.mmap', "r+") as f:
         mapf = mmap.mmap(f.fileno(),0)
         for dictFile in files:
-            inFile = open(dictFile,'r+b')
+            size = 0
+            inFile = open(dictFile)
             print "Preparing to load file", dictFile, "from pickle"
             loaded = cPickle.load(inFile); inFile.close()
             print "File loaded...."
